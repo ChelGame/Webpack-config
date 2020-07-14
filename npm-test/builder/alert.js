3d5a4e12863206ce63c7c7b6a750da9722e7a44c
ChelGame <53932772+ChelGame@users.noreply.github.com>
2020-07-14 20:59:49 +0300

    Add files via upload

---
+++ npm-test/builder/alert.js
@@ -0,0 +1,3 @@
+exports.alertHi = function (i) {
+    alert(i);
+};
---
+++ npm-test/builder/import.js
@@ -0,0 +1,5 @@
+import './css/style.css';
+import {alertHi} from './alert.js';
+
+alertHi(12);
+document.querySelector('body').innerHTML = "<div class='div'>Hello test!</div>";
